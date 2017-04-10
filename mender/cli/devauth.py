f381bf1d7843d59d8f55d18381d4a900e060dae6
Maciej Borzecki <maciej.borzecki@rndity.com>
2017-04-10 11:40:54 +0200

    cli/devauth: nicer printing of device info
    Signed-off-by: Maciej Borzecki <maciej.borzecki@rndity.com>

--- mender/cli/devauth.py
+++ mender/cli/devauth.py
@@ -19,6 +19,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import logging
+
 from mender.cli.utils import run_command, api_from_opts, do_simple_get, do_simple_delete
 from mender.client import authentication_url
 
@@ -48,15 +50,41 @@ def do_main(opts):
     run_command(opts.authcommand, commands, opts)
 
 
+def dump_device_brief(data):
+    logging.debug('device auth data: %r', data)
+    print('device ID: %s' % data['id'])
+    print('    created:  %s' % data['created_ts'])
+    print('    auth sets: %s' % ', '.join(['{} ({})'.format(aset['id'], aset['status'])
+                                          for aset in
+                                          data.get('auth_sets', [])]))
+
+def dump_device(data):
+    logging.debug('device auth data: %r', data)
+    print('device ID: %s' % data['id'])
+    print('    created:  %s' % data['created_ts'])
+    print('    auth sets:')
+    for aset in data.get('auth_sets', []):
+        print('       id: %s' % aset['id'])
+        print('       status: %s' % aset['status'])
+        print('       identity data: %s' % aset['id_data'])
+        key_lines = aset['pubkey'].split('\n')
+        print('       key:', key_lines[0])
+        for l in key_lines[1:]:
+            print(' ' * 11, l)
+
+
 def show_device(opts):
     url = authentication_url(opts.service, '/devices/{}'.format(opts.device))
     with api_from_opts(opts) as api:
-        rsp = do_simple_get(api, url)
+        rsp = do_simple_get(api, url,
+                            printer=lambda rsp: dump_device(rsp.json()))
 
 
 def list_devices(opts):
     with api_from_opts(opts) as api:
-        do_simple_get(api, authentication_url(opts.service, '/devices'))
+        do_simple_get(api, authentication_url(opts.service, '/devices'),
+                      printer=lambda rsp: [dump_device_brief(dev)
+                                           for dev in rsp.json()])
 
 def delete_device(opts):
     url = authentication_url(opts.service, '/devices/{}'.format(opts.device))
